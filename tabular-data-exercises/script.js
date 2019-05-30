200b33e48be6e4fb1c86ff29aedd5fb099278403
Ben Loper <loperbp@gmail.com>
2019-05-30 11:56:41 -0400

    Add Chart.JS to project, chart the years and temps from CSV file to display on the website

--- tabular-data-exercises/script.js
+++ tabular-data-exercises/script.js
@@ -9,21 +9,41 @@ getData().then(table => {
   //remove the first row
   rows.shift();
 
+  const labels = [];
+  const data = [];
+
   rows.forEach(row => {
     const columns = row.split(',');
 
-    const year = columns[0];
-    const temperature = columns[1];
-
-    addYearAndTempToPage(year, temperature);
+    labels.push(columns[0]);
+    data.push(columns[1]);
   });
+
+  generateChart(labels, data);
 });
 
-function addYearAndTempToPage(year, temperature) {
-  const tempsSection = document.getElementById('temps');
-  const pTag = document.createElement('p');
+function generateChart(years, temps) {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
 
-  pTag.innerText = `${year}: ${temperature}`;
+    // The data for our dataset
+    data: {
+      labels: years,
+      datasets: [
+        {
+          label: 'Change in Global Surface Temperature',
+          data: temps,
+          fill: false,
+          borderColor: '#922893',
+          borderWidth: 2,
+          pointBorderWidth: 1
+        }
+      ]
+    },
 
-  tempsSection.appendChild(pTag);
+    // Configuration options go here
+    options: {}
+  });
 }
